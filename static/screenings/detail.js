307da912b0dada65f92c1e2e5cdf3af1d5661405
velero <velero@musca.uberspace.de>
2015-01-07 23:24:07 +0100

    added static files

---
+++ static/screenings/detail.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+	$(".title").on("submit", function()
+	{
+		var film = $("");
+		
+		$.ajax({
+			url: "",
+			type: "POST",
+			data: { film: film },
+			success)
+	}
+	)
+}
+)
