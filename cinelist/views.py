6ca19819bae56975be27cb5365f26584facc8b17
David Falk <me@davidfalk.net>
2015-01-11 15:17:44 +0100

    added static files to index.html

--- cinelist/views.py
+++ cinelist/views.py
@@ -4,5 +4,7 @@ from cinelist.models import Cinema
 # Create your views here.
 def index(request):
     cinemalist = Cinema.objects.order_by('name')
-    context = {'cinemalist': cinemalist}
+    is_this_true = True
+    context = {'cinemalist': cinemalist,
+               'is_this_true': is_this_true}
     return render(request, 'cinelist/index.html', context)
